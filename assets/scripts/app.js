98a5c408ac9ea40af218f0298b7c05363fa4e6fc
mrtztrk <101321761+mrtztrk@users.noreply.github.com>
2022-04-17 15:16:11 +0300

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,45 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
+function sendHttpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.responseType = "json"; // parse for me to json data
+
+    xhr.onload = function () {
+      resolve(xhr.response);
+      //   const listOfPosts = JSON.parse(xhr.response);
+    };
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
+}
+
+async function fetchPosts() {
+  const responsData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = responsData;
+  for (post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchPosts();
+createPost("DUMMY", "A dummy post");
